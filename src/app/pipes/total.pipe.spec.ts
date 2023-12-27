de7874a2c27ba13ccb4f3249a19d6839efea10f4
Egemen Çiftci <egemenciftci@gmail.com>
2023-12-27 20:48:01 +0100

    .

--- src/app/pipes/total.pipe.spec.ts
+++ src/app/pipes/total.pipe.spec.ts
@@ -1,8 +1,10 @@
+import { inject } from '@angular/core';
+import { EntriesService } from '../services/entries.service';
 import { TotalPipe } from './total.pipe';
 
 describe('TotalPipe', () => {
   it('create an instance', () => {
-    const pipe = new TotalPipe();
+    const pipe = new TotalPipe(inject(EntriesService));
     expect(pipe).toBeTruthy();
   });
 });
