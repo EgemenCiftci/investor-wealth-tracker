c3180aa39a43ec89cd8a7ff67a68c05606d14eb1
Egemen Çiftci <egemenciftci@gmail.com>
2024-11-01 18:51:08 +0100

    ng generate @angular/core:control-flow

--- src/app/components/entries/entries.component.ts
+++ src/app/components/entries/entries.component.ts
@@ -10,7 +10,7 @@ import { Observable, Subject, map } from 'rxjs';
 import { AppState } from 'src/app/reducers/entries.reducer';
 import { MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, MatCardActions, MatCardFooter } from '@angular/material/card';
 import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, MatExpansionPanelDescription } from '@angular/material/expansion';
-import { NgFor, AsyncPipe, CurrencyPipe, DatePipe, KeyValuePipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe, DatePipe, KeyValuePipe } from '@angular/common';
 import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatDatepickerInput, MatDatepickerToggle, MatDatepicker } from '@angular/material/datepicker';
@@ -29,7 +29,7 @@ import { TotalPipe } from '../../pipes/total.pipe';
     templateUrl: './entries.component.html',
     styleUrls: ['./entries.component.css'],
     standalone: true,
-    imports: [MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, MatAccordion, NgFor, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, MatExpansionPanelDescription, MatFormField, MatLabel, MatInput, MatDatepickerInput, FormsModule, MatDatepickerToggle, MatSuffix, MatDatepicker, MatButton, MatIcon, MatSelect, MatOption, MatAutocompleteTrigger, MatAutocomplete, MatCardActions, MatCardFooter, MatProgressBar, AsyncPipe, CurrencyPipe, DatePipe, KeyValuePipe, CamelCaseToSpacesPipe, TotalPipe]
+    imports: [MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, MatExpansionPanelDescription, MatFormField, MatLabel, MatInput, MatDatepickerInput, FormsModule, MatDatepickerToggle, MatSuffix, MatDatepicker, MatButton, MatIcon, MatSelect, MatOption, MatAutocompleteTrigger, MatAutocomplete, MatCardActions, MatCardFooter, MatProgressBar, AsyncPipe, CurrencyPipe, DatePipe, KeyValuePipe, CamelCaseToSpacesPipe, TotalPipe]
 })
 export class EntriesComponent implements OnInit, OnDestroy {
   entries$ = this.store.select(x => x.entriesReducer.entries);
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,14 +6,14 @@ import { MatIconButton, MatButton, MatMiniFabButton } from '@angular/material/bu
 import { MatMenuTrigger, MatMenu, MatMenuItem } from '@angular/material/menu';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
-import { NgIf } from '@angular/common';
+
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
     standalone: true,
-    imports: [MatToolbar, MatIconButton, MatMenuTrigger, MatIcon, MatMenu, MatMenuItem, RouterLink, MatButton, RouterLinkActive, MatMiniFabButton, MatTooltip, NgIf]
+    imports: [MatToolbar, MatIconButton, MatMenuTrigger, MatIcon, MatMenu, MatMenuItem, RouterLink, MatButton, RouterLinkActive, MatMiniFabButton, MatTooltip]
 })
 export class HeaderComponent {
   photoUrl?: string;
