0b19b5ac501608dea02d6ec47cbdf79f7639b04e
Egemen Çiftci <egemenciftci@gmail.com>
2024-12-11 23:22:05 +0100

    test bug fixes

--- src/app/pipes/total.pipe.spec.ts
+++ src/app/pipes/total.pipe.spec.ts
@@ -2,9 +2,11 @@ import { inject } from '@angular/core';
 import { EntriesService } from '../services/entries.service';
 import { TotalPipe } from './total.pipe';
 
+const entriesService = inject(EntriesService);
+
 describe('TotalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TotalPipe(inject(EntriesService));
+  it('create an instance', (entriesService) => {
+    const pipe = new TotalPipe();
     expect(pipe).toBeTruthy();
   });
 });
